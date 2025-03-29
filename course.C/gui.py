29175f5cec19f462f309a9fb1448b3cff85de456
mikebstudy <mikebstudy@gmail.com>
2025-03-28 22:50:47 -0700

    C: Finish Edit button and Add functionality

--- course.C/gui.py
+++ course.C/gui.py
@@ -4,10 +4,12 @@ import FreeSimpleGUI as FSG
 label = FSG.Text("Type in a todo")
 input_box = FSG.InputText(tooltip="Enter todo", key="todo")
 add_button = FSG.Button("Add")
-edit_button = FSG.Button("Edit",key="todo_edit")
+list_box = FSG.Listbox(values=get_todos(), key="todos",
+                       enable_events=True, size=[45, 10])
+edit_button = FSG.Button("Edit")
 
 win = FSG.Window("ToDo List",
-                 layout=[[label],[input_box, add_button, edit_button]],
+                 layout=[[label],[input_box, add_button], [list_box, edit_button]],
                  font=("Helvetica",20))
 
 while True:
@@ -18,16 +20,24 @@ while True:
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             save_todos(todos)
+            win['todos'].update(values=todos)
 
         case "Edit":
             print("Edit button")
+            todo_to_edit = values['todos'][0]
+            new_todo = values["todo"]
+
             todos = get_todos()
-            new_todo = values["todo"] + "\n"
-            todos.append(new_todo)
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
             save_todos(todos)
+            win['todos'].update(values=todos)
+
+        case "todos":
+            win['todo'].update(value=values["todos"][0])
 
         case FSG.WIN_CLOSED:
-                break
+            break
 
 win.close()
 
