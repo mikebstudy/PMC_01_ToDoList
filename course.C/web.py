30dbc6a69c86c219061ff139e750cfa0c331007a
mikebstudy <mikebstudy@gmail.com>
2025-03-29 20:28:12 -0700

    C: Implement Add todo item feature

--- course.C/web.py
+++ course.C/web.py
@@ -1,13 +1,21 @@
 import streamlit as st
 from functions import get_todos, save_todos
 
+todos = get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    save_todos(todos)
+    st.session_state["new_todo"] = ""
+
+
 st.title("ToDo List")
 st.subheader("Course todo app")
 st.write("Increase your productivity!!!")
 
-todos = get_todos()
-
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="",placeholder="Add new todo...")
+st.text_input(label="",placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
